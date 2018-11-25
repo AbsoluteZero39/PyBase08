92c69d0e1c2a16cec3118bfe9f4d232a5420fd6a
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-11-25 13:16:00 +0200

    Update hw_1_3.py

--- hw_1_3.py
+++ hw_1_3.py
@@ -1,5 +1,11 @@
 a=int(input('a='))
 print(int(a),bool(a),float(a),str(a))
 b=int(input('b='))
-print(int(b)*int(a),bool(b)*bool(a),float(b)*float(a),bool(b)+bool(a),int(b)+int(a),float(b)+float(a),str(b)+str(a))
+print(int(b)*int(a),int(b)+int(a),int(b)-int(a),int(b)/int(a),int(b)**int(a),int(b)//int(a),int(b)%int(a))
+print(bool(b)*bool(a),bool(b)-bool(a),bool(b)+bool(a),bool(b)/bool(a))
+print(float(a)-float(b),float(a)+float(b),float(a)*float(b),float(a)/float(b),float(a)//float(b),float(a)**float(b))
+print(str(a)+str(b))
+print(int(b)+float(a),bool(a)+int(b),float(b)*int(a),float(a)/int(b),bool(b)*float(-b),int(b)**float(a))
+      
+
 
