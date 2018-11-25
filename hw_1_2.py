bb6d4dc9e3d71b7ab92038adc134791173129aec
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-11-25 12:13:51 +0200

    homework1

---
+++ hw_1_2.py
@@ -0,0 +1,3 @@
+a=int(input('a='))
+b=int(input('b='))
+print(a+b,a-b,a*b,a/b,a**b,a%b,a//b,int(a),float(a),bool(a),str(a))
---
+++ hw_1_3.py
@@ -0,0 +1,4 @@
+a=int(input('a='))
+print(int(a),bool(a),float(a),str(a))
+b=int(input('b='))
+print(b*int(a),b*bool(a),b*float(a),b+bool(a),b+int(a),b+float(a))
