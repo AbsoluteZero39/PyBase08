2038c8aa0fdd7c2a1790636524a9bbbd6bdeac6a
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-11-28 16:33:30 +0200

    Update hw_1_3.py

--- hw_1_3.py
+++ hw_1_3.py
@@ -1,11 +1,7 @@
 a=int(input('a='))
 print(int(a),bool(a),float(a),str(a))
 b=int(input('b='))
-print(int(b)*int(a),int(b)+int(a),int(b)-int(a),int(b)/int(a),int(b)**int(a),int(b)//int(a),int(b)%int(a))
-print(bool(b)*bool(a),bool(b)-bool(a),bool(b)+bool(a),bool(b)/bool(a))
-print(float(a)-float(b),float(a)+float(b),float(a)*float(b),float(a)/float(b),float(a)//float(b),float(a)**float(b))
-print(str(a)+str(b))
-print(int(b)+float(a),bool(a)+int(b),float(b)*int(a),float(a)/int(b),bool(b)*float(-b),int(b)**float(a))
-      
+print(int(b)*int(a),int(b)+int(a),float(a)+int(b),float(a)*int(a),float(a)*float(b),float(a)+float(b),bool(a)+bool(b),bool(a)*bool(b),bool(a)*int(b),bool(a)+int(b),bool(a)*float(b),bool(a)+float(b), str(a)+str(b))
+
 
 
