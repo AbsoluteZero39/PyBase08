c2e974fbcf2b155d648ede928a5a6b3f0e472a15
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-12-05 17:26:07 +0200

    Update hm_2_4.py

--- hm_2_4.py
+++ hm_2_4.py
@@ -3,11 +3,15 @@ u = 1
 v = 1
 k = 1
 print('номер числа Фибоначчи ',n)
-while k < n :
-    k = k + 1
-    t = u + v
-    u = v
-    v = t
+if n>0:
+    while k < n :
+        k = k + 1
+        t = u + v
+        u = v
+        v = t
+    
+    print ('Результат', u)      
+else:
+    print('put n>0')
 
 
-print ('Результат', u)
