ab9db63e80b243157ac1c2c485ea9915f80f5c58
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-12-05 17:13:31 +0200

    Update hm_2_2.py

--- hm_2_2.py
+++ hm_2_2.py
@@ -1,6 +1,12 @@
 a=float(input('a='))
 b=float(input('b='))
 k=0
-for i in range(int(round(a)),int(round(b))+1):
-    k=k+i
+if b>a:
+    for i in range(int(round(a)),int(round(b))+1):
+        k=k+i
     print(k)
+else:
+    for i in range(int(round(b)),int(round(a))+1):
+        k=k+i
+    print(k)
+print('sum elementov ot a do b')
