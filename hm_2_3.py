346b8e25a5dcd7f140478e29836c773a952a5bed
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-12-05 17:24:18 +0200

    Update hm_2_3.py

--- hm_2_3.py
+++ hm_2_3.py
@@ -1,7 +1,8 @@
 n = int(input('n= '))
 p = 1
-
-for i in range(1, n + 1):
-    p = p * i
-
-print(n, '!=', p)
+if n>=0:
+    for i in range(1, n + 1):
+        p = p * i
+else:
+    print('n<0, put another n')
+print(n, '!=', p, sep='')
