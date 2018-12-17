e733dfc71ccf93da737369e8f17a1559ccc3ae08
AbsoluteZero39 <45199423+AbsoluteZero39@users.noreply.github.com>
2018-12-17 17:11:20 +0200

    Add files via upload

--- hw_4_1.py
+++ hw_4_1.py
@@ -1,14 +1,19 @@
 def clear_word(word, filterstr):
-    word=word.replace(filterstr,'')
+    for i in filterstr:
+        word=word.replace(i,' ')
     result=word
     return result
-a=input('введите ваше слово')
-b=input('введите не нужный символ')
-for i in a:
-    if ord(i)>=65 and ord(i)<=122:
-        k=True
-    else:
-        m=a.index(i)
-        raise ValueError(i,'- наш символ',m,'- индекс нашего неверного символа')
-x=clear_word(a,b)
-print(x)
+try:
+    a=input('введите ваше слово')
+    b=input('введите не нужный символ')
+    for i in a:
+        if ord(i)>=65 and ord(i)<=122:
+            k=True
+        else:
+            m=a.index(i)
+            raise ValueError
+    x=clear_word(a,b)
+    print(x)
+except ValueError:
+    print(i,'- наш символ',m,'- индекс нашего неверного символа')
+    print(x)
